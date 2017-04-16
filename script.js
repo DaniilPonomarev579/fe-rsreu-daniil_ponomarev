d102a0b97c6d4e2ac7d72e20cfb23e094ecb6974
DaniilPonomarev579 <daniil.ponomarev579@gmail.com>
2017-04-16 18:19:27 +0300

    Added tasks

--- script.js
+++ script.js
@@ -405,6 +405,7 @@ var library = [
 	{author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
 	{author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
 ];
+
 //console.log(library.sort(sortByTitle));
 
 
