fe9f70ac9d19e08e5fb4213cbe30caa35fc7c58e
DaniilPonomarev579 <daniil.ponomarev579@gmail.com>
2017-04-21 10:07:43 +0300

    Initial commit

--- script.js
+++ script.js
@@ -1,144 +1,96 @@
 /**
- * Created by Danil on 11.04.2017.
+ * Created by Danil on 19.04.2017.
  */
 
-function isNumber(value) {
-	return typeof value === 'number' && isFinite(value);
-}
-
-function isArray(array) {
-	return Object.prototype.toString.call(array) === '[object Array]';
-}
-
-function calcTriangleArea(a, h) {
-	"use strict";
-	
-	if (!isNumber(a) || !isNumber(h)) {
-		throw new Error('a and h should be numbers');
-	}
-	
-	if (a < 0 || h < 0) {
-		throw new Error('a and h should be > 0');
-	}
-	
-	return (a * h / 2);
-}
-
-function reverseArrayWithFor(array) {
+var Utils = (function () {
 	'use strict';
 	
-	if (!isArray(array)) {
-		return 'not an array';
+	function isNumber(value) {
+		return typeof value === 'number' && !isNaN(value);
 	}
 	
-	for (i = 0; i < array.length / 2; i++) {
-		var t = array[i];
-		array[i] = array[array.length - i - 1];
-		array[array.length - i - 1] = t;
+	return {
+		isNumber: isNumber
+	};
+}());
+
+var Calculator = (function (Utils) {
+	'use strict';
+	
+	var result = 0;
+	
+	function add() {
+		for (let i = 0; i < arguments.length; i++) {
+			if (!Utils.isNumber(arguments[i])) {
+				throw new Error('Not a Number');
+			}
+			
+			result += arguments[i];
+		}
+		
+		return add;
 	}
 	
-	return array;
-}
-
-function reverseArrayWithWhile(array) {
-	"use strict";
-	
-	if (!isArray(array)) return 'not an array';
-	
-	while (i != 0) {
-		i--;
-		var t = array[i];
-		array[i] = array[array.length - i - 1];
-		array[array.length - i - 1] = t;
+	function subtract() {
+		for (var i = 0; i < arguments.length; i++) {
+			if (!Utils.isNumber(arguments[i])) {
+				throw new Error('Not a Number');
+			}
+			
+			result -= arguments[i];
+		}
+		
+		return subtract;
 	}
 	
-	return array;
-}
-
-function reverseArrayWithDoWhile(array) {
-	"use strict";
-	
-	if (!isArray(array)) {
-		return 'not an array';
+	function divide() {
+		for (var i = 0; i < arguments.length; i++) {
+			if (!Utils.isNumber(arguments[i])) {
+				throw new Error('Not a Number');
+			}
+			
+			result /= arguments[i];
+		}
+		
+		return divide;
 	}
 	
-	do {
-		var t = array[i];
-		array[i] = array[array.length - i - 1];
-		array[array.length - i - 1] = t;
-		i++;
-	} while (i < array.length / 2);
-	
-	return array;
-}
-
-function explainIncrementDifference() {
-	"use strict";
-	
-	var i1 = 0;
-	var i2 = 0;
-	
-	console.log('Initial values: ' + i1 + ', ' + i2);
-	console.log('Intermediate values: ' + i1++ + ', ' + ++i2);
-	console.log('Final values: ' + i1 + ', ' + i2);
-}
-
-function checkNumberSign(number) {
-	"use strict";
-	
-	if (!isNumber(number)) return 'not a number';
-	
-	if (number > 0) {
-		return 'positive';
-	} else if (number < 0) {
-		return 'negative';
-	} else if (number === 0) {
-		return 'zero';
-	}
-}
-
-function countFactorial(number) {
-	"use strict";
-	
-	if (!isNumber(number)) {
-		throw new Error('Can\'t count the factorial');
+	function multiply() {
+		for (let i = 0; i < arguments.length; i++) {
+			if (!Utils.isNumber(arguments[i])) {
+				throw new Error('Not a Number');
+			}
+			
+			result *= arguments[i];
+		}
+		
+		return multiply;
 	}
 	
-	if (number===0) {
-		return 1;
+	function getResult() {
+		return result;
 	}
 	
-	if (number<0) {
-		throw new Error('The number is negative!');
+	function reset() {
+		result = 0;
 	}
 	
-	return number === 0 ?  1 :  number*countFactorial(number-1);
-}
+	return {
+		add: add,
+		subtract: subtract,
+		divide: divide,
+		multiply: multiply,
+		getResult: getResult,
+		reset: reset
+	};
+})(Utils);
 
-console.log(countFactorial(5));
-console.log(countFactorial(10));
-console.log(countFactorial(0));
-
-var a = 5;
-var h = 10;
-console.log('Triangle side = ' + a + ';\nTriangle height = ' + h);
-console.log('Triangle area = ' + calcTriangleArea(a, h));
-
-var array = [];
-for (var i = 0; i < 10; i++) {
-	array[i] = i;
-}
-console.log('Initial array: ' + array);
-console.log('Reversed array 1: ' + reverseArrayWithFor(array));
-console.log('Reversed array 2: ' + reverseArrayWithWhile(array));
-console.log('Reversed array 3: ' + reverseArrayWithDoWhile(array));
-
-explainIncrementDifference();
-
-var number = 3;
-console.log('The number is ' + checkNumberSign(number));
-
-var name = prompt('What\'s your name?');
-alert(name);
-
-console.log('Factorial of ' + number + ' = ' + countFactorial(number));
+Calculator.reset();
+Calculator.add(5);
+Calculator.add(5, 5, 5);
+Calculator.reset();
+Calculator.add(5)(5);
+// Calculator.subtract(10);
+Calculator.multiply(5, 2)(10);
+// Calculator.divide(10);
+console.log(Calculator.getResult());
